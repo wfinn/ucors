e551dd6853cbed4dfff0277f697350f5033f69d2
Finn Westendorf <fw@sernet.de>
2022-04-17 15:52:05 +0200

    import from stuff repo, edit README

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/wfinn/ucors
+
+go 1.18
+
+require github.com/wfinn/go-tld v1.1.2-0.20220417134809-a06deb5c36b2
+
+require golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
---
+++ main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
+	"sync"
+	"time"
+
+	tld "github.com/wfinn/go-tld"
+)
+
+var cookie string
+var printonly bool
+
+func main() {
+	goroutines := flag.Uint("r", 20, "go routines")
+	flag.StringVar(&cookie, "c", "", "cookie e.g. session=abc123")
+	flag.BoolVar(&printonly, "p", false, "only print the payloads")
+	flag.Parse()
+	urls := make(chan string)
+
+	// workers
+	var wg sync.WaitGroup
+	for i := 0; i < int(*goroutines); i++ {
+		wg.Add(1)
+
+		c := getClient()
+		go func() {
+			defer wg.Done()
+
+			for u := range urls {
+				testOrigins(c, u)
+			}
+		}()
+	}
+
+	sc := bufio.NewScanner(os.Stdin)
+
+	// read urls from command line
+	for _, u := range flag.Args() {
+		urls <- u
+	}
+	// read urls from stdin
+	for sc.Scan() {
+		urls <- sc.Text()
+	}
+	close(urls)
+
+	wg.Wait()
+
+}
+
+func getClient() *http.Client {
+	tr := &http.Transport{
+		MaxIdleConns:    30,
+		IdleConnTimeout: time.Second,
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		DialContext: (&net.Dialer{
+			Timeout:   time.Second * 10,
+			KeepAlive: time.Second,
+		}).DialContext,
+	}
+
+	re := func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	return &http.Client{
+		Transport:     tr,
+		CheckRedirect: re,
+		Timeout:       time.Second * 10,
+	}
+}
+
+func testOrigins(c *http.Client, u string) {
+	pp, err := getPermutations(u)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return
+	}
+
+	for _, p := range pp {
+		if printonly {
+			fmt.Println(p)
+			continue
+		}
+		req, err := http.NewRequest("GET", u, nil)
+		if err != nil {
+			return
+		}
+		req.Header.Set("Origin", p)
+		if cookie != "" {
+			req.Header.Set("Cookie", cookie)
+		}
+		resp, err := c.Do(req)
+		if resp != nil {
+			io.Copy(io.Discard, resp.Body)
+			resp.Body.Close()
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error requesting %s: %s\n", u, err)
+			return
+		}
+
+		acao := resp.Header.Get("Access-Control-Allow-Origin")
+		acac := resp.Header.Get("Access-Control-Allow-Credentials")
+
+		if acao == p {
+			fmt.Printf("Url: %s Origin: %s ACAC: %s\n", u, p, acac)
+		}
+	}
+}
+
+func getPermutations(raw string) ([]string, error) {
+	u, err := url.Parse(raw)
+	if err != nil {
+		return []string{}, err
+	}
+
+	origins := []string{
+		"https://evil.com",
+		"http://evil.com",
+		"null",
+	}
+
+	patterns := []string{
+		"https://%s.evil.com",
+		"https://%sevil.com",
+		"https://xssonanysubdomain.%s",
+	}
+
+	for i, p := range patterns {
+		patterns[i] = fmt.Sprintf(p, u.Hostname())
+	}
+	origins = append(origins, patterns...)
+
+	origin := u.Scheme + "://" + u.Host
+
+	if u, err := tld.Parse(raw); err == nil {
+		// e.g. https://wwwXtarget.tld
+		if u.Subdomain != "" {
+			origins = append(origins, strings.Replace(origin, ".", "x", 1))
+		} else {
+			origins = append(origins, "https://"+"wwwx"+u.Hostname())
+		}
+		// e.g. https://target.wtf
+		if re, err := regexp.Compile("\\." + u.TLD); err == nil {
+			newTLD := ".wtf"
+			if u.TLD == newTLD {
+				newTLD = ".ooo"
+			}
+			origins = append(origins, re.ReplaceAllString(origin, newTLD))
+		}
+	}
+
+	//most of these only work on Safari like with redirex
+	subdomainchars := []string{",", "&", "'", "\"", ";", "!", "$", "^", "*", "(", ")", "+", "`", "~", "-", "_", "=", "|", "{", "}", "%", "%01", "%02", "%03", "%04", "%05", "%06", "%07", "%08", "%0b", "%0c", "%0e", "%0f", "%10", "%11", "%12", "%13", "%14", "%15", "%16", "%17", "%18", "%19", "%1a", "%1b", "%1c", "%1d", "%1e", "%1f", "%7f"}
+	for _, char := range subdomainchars {
+		// e.g. https://target.tld&.evil.com
+		origins = append(origins, origin+char+".evil.com")
+	}
+	return origins, nil
+}
